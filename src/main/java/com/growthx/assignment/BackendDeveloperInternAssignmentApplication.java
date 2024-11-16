f6892bc7db9e6bc868943aa9d4468f7f055d8f5f
GurdeepOnsurity <gss100009@gmail.com>
2024-11-16 11:22:46 +0530

    initial commits

---
+++ src/main/java/com/growthx/assignment/BackendDeveloperInternAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.growthx.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperInternAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperInternAssignmentApplication.class, args);
+	}
+
+}
